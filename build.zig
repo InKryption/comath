9a04ecb3a62d40997e28fc5382f78c703b260184
InKryption <inkryption07@gmail.com>
2023-08-08 13:56:43 +0200

    Change build.zig to reflect new organisation

--- build.zig
+++ build.zig
@@ -5,20 +5,13 @@ pub fn build(b: *Build) void {
     const util_mod = b.createModule(.{
         .source_file = Build.FileSource.relative("src/util.zig"),
     });
-    const eval = b.addModule("eval", .{
+    const eval_mod = b.addModule("eval", .{
         .source_file = Build.FileSource.relative("src/eval.zig"),
         .dependencies = &.{
             .{ .name = "util", .module = util_mod },
         },
     });
-    const contexts = b.addModule("contexts", .{
-        .source_file = Build.FileSource.relative("src/contexts.zig"),
-        .dependencies = &.{
-            .{ .name = "util", .module = util_mod },
-            .{ .name = "eval", .module = eval },
-        },
-    });
-    _ = contexts;
+    _ = eval_mod;
 
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
@@ -31,16 +24,6 @@ pub fn build(b: *Build) void {
     const run_main_tests = b.addRunArtifact(main_tests);
     main_tests.addModule("util", util_mod);
 
-    const contexts_tests = b.addTest(.{
-        .root_source_file = Build.FileSource.relative("src/contexts.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    const run_contexts_tests = b.addRunArtifact(contexts_tests);
-    contexts_tests.addModule("eval", eval);
-    contexts_tests.addModule("util", util_mod);
-
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&run_main_tests.step);
-    test_step.dependOn(&run_contexts_tests.step);
 }
