cf201a299ddfff0afc88a8f2c3a06ea9e5da4a6e
InKryption <inkryption07@gmail.com>
2024-11-26 16:25:19 +0100

    Improve build.zig

--- build.zig
+++ build.zig
@@ -4,10 +4,15 @@ const Build = std.Build;
 pub fn build(b: *Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const run_steps = !(b.option(bool, "no-run", "Don't run any of the executables implied by the specified steps.") orelse false);
 
-    const test_step = b.step("test", "Run all tests");
-    const unit_test_step = b.step("unit-test", "Run unit tests");
-    test_step.dependOn(unit_test_step);
+    const install_step = b.getInstallStep();
+    const unit_test_step = b.step("unit-test", "Run unit tests.");
+
+    {
+        const test_step = b.step("test", "Run all tests.");
+        test_step.dependOn(unit_test_step);
+    }
 
     const comath_mod = b.addModule("comath", .{
         .root_source_file = b.path("src/main.zig"),
@@ -17,12 +22,18 @@ pub fn build(b: *Build) void {
     });
     comath_mod.addImport("util", util_mod);
 
-    const unit_tests = b.addTest(.{
+    const unit_test_exe = b.addTest(.{
+        .name = "unit-test",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
-    unit_tests.root_module.addImport("util", util_mod);
-    const run_unit_tests = b.addRunArtifact(unit_tests);
-    unit_test_step.dependOn(&run_unit_tests.step);
+    unit_test_exe.root_module.addImport("util", util_mod);
+
+    const unit_test_install = b.addInstallArtifact(unit_test_exe, .{});
+    install_step.dependOn(&unit_test_install.step);
+    install_step.dependOn(&unit_test_install.step);
+
+    const unit_test_run = b.addRunArtifact(unit_test_exe);
+    if (run_steps) unit_test_step.dependOn(&unit_test_run.step);
 }
